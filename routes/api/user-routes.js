a08203c82ae0fd71f411d98ab8509ebafb0b555f
Jatin_Saini <sainijatin247@gmail.com>
2022-09-26 19:02:18 -0400

    comment routes added

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { User } = require("../../models");
+const { User, Post, Vote } = require("../../models");
 
 // GET /api/users
 router.get("/", (req, res) => {
@@ -17,6 +17,26 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   User.findOne({
     attributes: { exclude: ["password"] },
+    include: [
+      {
+        model: Post,
+        attributes: ['id', 'title', 'post_url', 'created_at']
+      },
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'created_at'],
+        include: {
+          model: Post,
+          attributes: ['title']
+        }
+      },
+      {
+        model: Post,
+        attributes: ['title'],
+        through: Vote,
+        as: 'voted_posts'
+      }
+    ],
     where: {
       id: req.params.id,
     },
